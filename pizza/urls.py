17b105e1851af81ffdec145a60d58a13c93ab98a
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2023-05-13 13:16:21 +0200

    Update urls.py

--- pizza/urls.py
+++ pizza/urls.py
@@ -13,3 +13,6 @@ urlpatterns = [
     path('test/',views.TestView.as_view(),name="test"),
     path('image/',views.ImageView.as_view(),name='image'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
